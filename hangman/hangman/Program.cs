9d38a39ca6b5bc7addb7df979e32b12501e7db63
Melissa Flores <106169198+Meli357@users.noreply.github.com>
2023-12-04 13:05:12 +0100

    declare variables for initial scheme of game

---
+++ hangman/hangman/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //list of words that the user has to guess
+            string[] words = { "csharp", "visualstudio", "language", "array", "variable", "parsing", "constant" };
+
+            //to verify that the player's input is within 'a' and 'z'
+            var validCharacters = new Regex("^[a-z]$");
+
+            //find a random number
+            int myRandomNumber = new Random().Next(0, words.Length - 1);
+
+            //pick a random word from the array
+            string wordToGuess = words[myRandomNumber];
+
+            //number of lives
+            int lives = 3;
+
+            //list of letters submitted by the player
+            List<string> letters = new();
+
+            //as long as the player has lives left, the game continues
+            while (lives != 0)
+            {
+            }
+        }
+    }
+}
